524c2d682015719c127882a4c623feea5fc52f61
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 15:04:20 +0200

    images

---
+++ sanity-backend/frontend_react/src/constants/images.js
@@ -0,0 +1,57 @@
+import email from '../assets/email.png';
+
+import api from '../assets/api.png';
+import cpp from '../assets/cpp.png';
+import css from '../assets/css.png';
+import figma from '../assets/figma.png';
+
+import git from '../assets/git.png';
+
+import html from '../assets/html.png';
+import javascript from '../assets/javascript.png';
+import node from '../assets/node.png';
+import react from '../assets/react.png';
+import sass from '../assets/sass.png';
+import typescript from '../assets/typescript.png';
+import vue from '../assets/vue.png';
+
+import about01 from '../assets/about01.png';
+import about02 from '../assets/about02.png';
+import about03 from '../assets/about03.png';
+import about04 from '../assets/about04.png';
+
+
+import circle from '../assets/circle.svg';
+
+import joshua from '../assets/joshua.png'
+import adidas from '../assets/adidas.png';
+import amazon from '../assets/amazon.png';
+import asus from '../assets/asus.png';
+import bolt from '../assets/bolt.png';
+
+
+export default {
+  email,
+  api,
+  cpp,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  node,
+  react,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  circle,
+  adidas,
+  joshua,
+  amazon,
+  asus,
+  bolt, 
+};
