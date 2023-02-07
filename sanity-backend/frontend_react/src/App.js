27f1293677b4fb6104cd5f2ee8ce0b34093e2b19
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 16:24:23 +0200

    App.js

--- sanity-backend/frontend_react/src/App.js
+++ sanity-backend/frontend_react/src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
+import './App.scss';
+
+// import { About, Footer, Header, Skills, Testimonial, Work } from './container';
+import { Navbar } from './components/Navbar';
 
 
-function App() {
+const App = () => {
   return (
-    <h1>App</h1>
-  );
+    <div className="app">
+      <Navbar />
+       {/* <Header />
+       <About />
+       <Work />
+       <Skills />
+       <Testimonial />
+       <Footer /> */}
+      
+     </div>
+   );
 }
-
-export default App;
+ export default App;
