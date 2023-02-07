d8c350e72141f6ba60f7963df3e00b6d68158ded
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-07 13:49:57 +0200

    components setup

---
+++ sanity-backend/frontend_react/src/components/Testimonial/Testimonial.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Testimonial.scss'
+
+const Testimonial = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default Testimonial
---
+++ sanity-backend/frontend_react/src/components/Work/Work.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './About.scss'
+
+const Work = () => {
+  return (
+    <div>About</div>
+  )
+}
+
+export default Work
