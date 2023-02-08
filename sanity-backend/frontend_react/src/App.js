9721bb40632857e0fa0173d1f91f16a3982b9954
joshbam-joshwilly <Alanajoshua535@gmail.com>
2023-02-08 12:30:10 +0200

    about section

--- sanity-backend/frontend_react/src/App.js
+++ sanity-backend/frontend_react/src/App.js
@@ -10,7 +10,7 @@ const App = () => {
     <div className="app">
       <Navbar />
         <Header />
-       {/* <About /> */}
+       <About />
        {/* <Work /> */}
        {/* <Skills /> */}
        {/* <Testimonial /> */}
